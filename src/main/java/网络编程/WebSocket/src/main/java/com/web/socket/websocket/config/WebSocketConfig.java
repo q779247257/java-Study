02a3c1e12ef8d0930ebe19a44cf9e8bc3345ff9d
git_xuanxuan <779247257@qq.com>
2020-02-20 14:07:35 +0800

    增加Spring Boot  整合 WebSocket 聊天demo

---
+++ src/main/java/网络编程/WebSocket/src/main/java/com/web/socket/websocket/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.web.socket.websocket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Author: 轩轩
+ * @Date: 2020/2/20 13:35
+ * @description: 2020年2月20日
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
