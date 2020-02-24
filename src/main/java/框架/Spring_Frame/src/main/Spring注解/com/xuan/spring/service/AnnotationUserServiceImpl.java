3b8b522f6a5a683b44d7c6e593b3e48d3461802b
git_xuanxuan <779247257@qq.com>
2020-02-24 11:16:38 +0800

    增加 Spring @Autowired//根据类型自动注入

--- src/main/java/框架/Spring_Frame/src/main/Spring注解/com/xuan/spring/service/AnnotationUserServiceImpl.java
+++ src/main/java/框架/Spring_Frame/src/main/Spring注解/com/xuan/spring/service/AnnotationUserServiceImpl.java
@@ -1,9 +1,10 @@
 package com.xuan.spring.service;
 
 import com.xuan.spring.dao.AnnotationUserDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 
 /**
  * @Author: 轩轩
@@ -12,7 +13,10 @@ import javax.annotation.Resource;
  */
 @Component("userServiceImpl")
 public class AnnotationUserServiceImpl {
-    @Resource(name = "userDao")
+//    @Resource(name = "userDao")
+
+    @Autowired//根据类型自动注入
+//    @Qualifier("userDao")//指定名称
     private AnnotationUserDaoImpl userDao;
 
     public void  saveUser(){
