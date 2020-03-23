12acb154a6c47a28fb71183d4c3c9036a6bcd63a
git_xuanxuan <779247257@qq.com>
2020-03-23 14:35:23 +0800

    增加lambda表达式工具类 steam流遍历 获取 foeach中获取元素所属下标

--- src/main/java/基础/Lambda表达式/Test.java
+++ src/main/java/基础/Lambda表达式/Test.java
@@ -1,5 +1,8 @@
 package 基础.Lambda表达式;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: 轩轩
  * @Date: 2019/12/28 19:02
@@ -12,5 +15,22 @@ public class Test {
         LamvdaTest number2 = (a,b,c) -> a+b+c;
 
         System.out.println(number2.sum2(2,2,2));
+    }
+
+    /**
+     * steam流遍历 或者 foreach中获取元素和下标
+     */
+    @org.junit.Test
+    public void test001(){
+        List<String> list = new ArrayList<>();
+        //在集合中添加元素
+        for ( int i = 0 ; i<=200 ; i++){
+            list.add(""+i);
+        }
+        list.stream().forEach(LambdaUtils.consumerWithIndex((item , index) -> {
+            System.out.println("下标为"+index);
+        }));
+        System.out.println("list的长度为:"+list.size());
+        System.out.println("info:"+list.get(list.size()-1));
     }
 }
