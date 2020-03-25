dc188147601ed315bb856ce7977e3d99fbcc6fb2
git_xuanxuan <779247257@qq.com>
2020-03-25 18:41:30 +0800

    增加算法  回文数

--- src/main/java/算法/AlgorithmCode.java
+++ src/main/java/算法/AlgorithmCode.java
@@ -7,18 +7,19 @@ package 算法;
  */
 public class AlgorithmCode {
     public static void main(String[] args) {
-        int reverse = new AlgorithmCode().reverse(1534236469);
-        System.out.println("info:"+
-                        (-123 / 10)
-                );
-        System.out.println("结果："+reverse);
+        boolean palindrome = new AlgorithmCode().isPalindrome(10);
+        System.out.println("结果"+palindrome);
+
     }
     //todo 整数反转 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
     public int reverse(int number) {
         long resultNumber = 0;
         while (number != 0){
             //判断数据溢出
-            if ( resultNumber >  Integer.MAX_VALUE /10 || (long)resultNumber < Integer.MIN_VALUE /10) return 0;
+            if ( resultNumber >  Integer.MAX_VALUE /10 || (long)resultNumber < Integer.MIN_VALUE /10) {
+                System.out.println("反转之后，超出了int的范围");
+                return 0;
+            }
             //取得最后一位数
            int add =  number % 10;
            //每次循环乘10 加上最后一位数
@@ -28,5 +29,31 @@ public class AlgorithmCode {
             //判断数据是否溢出
         }
         return (int) resultNumber;
+    }
+
+    // todo 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。(回文数算法)
+    /**
+     * 输入: -121
+     * 输出: false
+     * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     *
+     * 输入: 121
+     * 输出: true
+     * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+     */
+    public boolean isPalindrome(int number) {
+        int sum = number;
+        int resultNumber = 0;
+        while (number > 0){
+            //  每次取最后一位数
+            int add =  number % 10;
+            //当数*10（进一位）加上读取的最后一位数
+            resultNumber = resultNumber * 10 + add;
+            //往后推一位数
+            number =  number / 10;
+        }
+        System.out.println("sum:"+sum);
+        System.out.println("resultNumber:"+resultNumber);
+        return resultNumber == sum;
     }
 }
